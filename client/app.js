a6f659f6441b0a009223c523002aab6dca35eec3
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2025-02-06 19:12:20 +0000

    Init project

---
+++ client/app.js
@@ -0,0 +1,15 @@
+{
+  const loginForm = document.getElementById('welcome-form');
+  const messagesSection = document.getElementById('messages-section');
+  const messageList = document.getElementById('messages-list');
+  const addMessageForm = document.getElementById('add-messages-form');
+  const userNameInput = document.getElementById('username');
+  const messageContentInput = document.getElementById('message-content');
+
+  console.log('loginForm', loginForm);
+  console.log('messagesSection', messagesSection);
+  console.log('messageList', messageList);
+  console.log('addMessageForm', addMessageForm);
+  console.log('userNameInput', userNameInput);
+  console.log('messageContentInput', messageContentInput);
+}
