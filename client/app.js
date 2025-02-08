ac0e6eed03e5fbd2a5217fc648805ea0ba57d1c4
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2025-02-08 17:38:58 +0000

    Add basic server code

--- client/app.js
+++ client/app.js
@@ -8,16 +8,48 @@
 
   let userName = '';
 
-  const login = () => {
-    if(userNameInput.value.length === 0) {
-      prompt('Please give your name');
+  const login = (e) => {
+    e.preventDefault();
+    if(!userNameInput.value) {
+      alert('Please give your name');
     } else {
       userName = userNameInput.value;
+      loginForm.classList.remove('show');
+      messagesSection.classList.add('show');
     }
   }
 
-  loginForm.addEventListener( e => {
+  const addMessage = (author, content) => {
+    const message = document.createElement('li'); 
+    message.classList.add('message', 'message--received');
+    if(author === userName) message.classList.add('message--self');
+
+    const headerAuthor = document.createElement('h3');
+    headerAuthor.classList.add('message', 'message__author');
+    author === userName ? author = 'you' : author;
+    headerAuthor.textContent = author;
+
+    const newDiv = document.createElement('div'); 
+    newDiv.classList.add('message__content');
+    newDiv.textContent = content;
+
+    message.appendChild(headerAuthor); 
+    message.appendChild(newDiv);
+
+    messageList.appendChild(message);
+  }
+
+  const sendMessage = (e) => {
     e.preventDefault();
-    login();
-  })
+    if(!messageContentInput.value) {
+      alert('no message to send');
+    } else {
+      addMessage(userName, messageContentInput.value);
+      messageContentInput.value = '';
+    }
+  }
+
+  loginForm.addEventListener('submit', login);
+
+  addMessageForm.addEventListener('submit', sendMessage);
 }
